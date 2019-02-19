7ee9e43a732af6351e8339149a21c4895bc184ce
MeenPongpanit <61070124@kmitl.ac.th>
2019-02-19 18:39:41 +0700

    2/19/2019

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
 var button = {};
 var pressing = "";
-var speed = 2;
+var speed = 1.5;
 var keybutton = {"C":"A", "D":"S", "E":"D", "F":"F", "G":"J", "A":"K", "B":"L", "C2":String.fromCharCode(186), "S1":"W", "S2":"E", "S3":"U", "S4":"I", "S5":"O"};
 var tilewidth = {"TC":50, "TS1":25, "TD":50, "TS2":25, "TE":50, "TF":50, "TS3":25, "TG":50, "TS4":25, "TA":50, "TS5":25, "TB":50, "TC2":50};
+var score = 0;
+var presscount = 0;
+var notedowned = 0;
 
 document.onkeydown = document.onkeyup = function(e){
     var key_code = String.fromCharCode(e.keyCode);
@@ -12,11 +15,15 @@ document.onkeydown = document.onkeyup = function(e){
             pressing += i + " ";
         }
     }
+    presscount += e.type == "keyup";
     // keypress.innerText = pressing;
     pressing = "";
 }
 var allkeyid = {0:"C", 1:"D", 2:"E", 3:"F", 4:"G", 5:"A", 6:"B", 7:"C2", 8:"S1", 9:"S2", 10:"S3", 11:"S4", 12:"S5", 13:"S6"};
 
+// document.onkeyup = function(){
+//     presscount += 1;
+// }
 
 for(i = 0 ; i <= 13 ; i++){
     var pk = document.getElementById(allkeyid[i])
@@ -63,13 +70,17 @@ function addnote(tileid, rythm){
     fallingnote.push(note);
     tile = document.getElementById(tileid);
     tile.appendChild(note);
+    notedowned += 1;
 }
-var song = [["C", 1, 0], ["D", 0.5, 500], ["E", 2, 2500], ["S1", 1, 3500]];
-
+var song = [["C", 1], ["D", 0.5], ["E", 2], ["S1", 4], ["B", 3]];
+var songtime = 0;
 for(let i = 0 ; i <= song.length - 1 ; i++){
+    songtime += song[i][1]*(i != 0);
+    console.log(songtime*1000/Math.pow(speed, 2));
     setTimeout(function(){
         addnote("T" + song[i][0], song[i][1]);
-    }, song[i][2]/Math.pow(speed, 2));
+
+    }, songtime*1000/Math.pow(speed, 2));
 }
 
 function falldownnote(noteid){
@@ -90,7 +101,7 @@ function delunbound(noteid){
 function isunboundnote(noteid){
     var note = fallingnote[noteid];
     var posy = Number(note.getAttribute("y"));
-    return (posy >= 450)
+    return (posy >= 480)
 }
 
 function keyhit(){
@@ -99,9 +110,10 @@ function keyhit(){
         var posy = fallingnote[i].getAttribute("y");
         var notesize = Number(fallingnote[i].getAttribute("size"))*20;
         var canhit = (posy >= (450 - notesize)) && (posy <= 450);
-        // keypress.innerText = canhit && button[keybutton[fallingnote[i].getAttribute("keytohit")]];
+        keypress.innerText = canhit && button[keybutton[fallingnote[i].getAttribute("keytohit")]];
         if(canhit && button[keybutton[fallingnote[i].getAttribute("keytohit")]]){
             fallingnote[i].setAttribute("hit", "yes");
+            score += 10;
         }
     }
 }
@@ -116,5 +128,9 @@ setInterval(function(){
         keyhit();
     }
 }
-keypress.innerText = JSON.stringify(button);
+// keypress.innerText = score;
+accuracy.innerText = presscount;
 }, 50/speed)
+//make wrongpress condition
+//make red bg color tiles when wrong hit
+//reset status of tile when tile's key up
